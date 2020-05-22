3568f0620a386b1bfc8754482b27a072be7cde4b
devwares <dev@wares.fr>
2020-05-22 11:40:41 +0200

    SET-EXECUTIONPOLICY - added commented line : set execution policy to unrestricted for current user

--- Scripts/Call-Ps1.bat
+++ Scripts/Call-Ps1.bat
@@ -1,6 +1,7 @@
 @echo off
 REM cd %~dp0
 REM REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
+REM Set-ExecutionPolicy -Scope "CurrentUser" -ExecutionPolicy "Unrestricted"
 PowerShell -ExecutionPolicy Bypass -File "%~dp0scriptname.ps1"
 echo Terminated.
 pause
