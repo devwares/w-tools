e8cb0fe2f977b15beedfc18f8b9f684a69f21078
Dev <dev@wares.fr>
2020-05-25 18:01:54 +0200

    AJOUT EXEMPLES Call-Ps1.bat

--- Scripts/Call-Ps1.bat
+++ Scripts/Call-Ps1.bat
@@ -1,7 +1,11 @@
 @echo off
+
 REM cd %~dp0
 REM REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 REM Set-ExecutionPolicy -Scope "CurrentUser" -ExecutionPolicy "Unrestricted"
+REM Set-ExecutionPolicy Unrestricted
+REM powershell.exe -ExecutionPolicy Unrestricted -File file.ps1
+
 PowerShell -ExecutionPolicy Bypass -File "%~dp0scriptname.ps1"
 echo Terminated.
 pause
