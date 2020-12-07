f98c36df736355a9028a3b70b6a22c3358a2c512
Dev <dev@wares.fr>
2020-12-07 18:42:51 +0100

    POWERSHELL SCRIPT CALLS EXAMPLES - added examples/set-executionpolicy.ps1 - updated examples/Call-Ps1.bat

--- Scripts/batch/examples/Call-Ps1.bat
+++ Scripts/batch/examples/Call-Ps1.bat
@@ -1,11 +1,13 @@
 @echo off
+SET SCRIPTHOME=%~dp0
+SET SCRIPTNAME=script.ps1
+SET SCRIPTPATH=%SCRIPTHOME%%SCRIPTNAME%
 
-REM cd %~dp0
-REM REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
-REM Set-ExecutionPolicy -Scope "CurrentUser" -ExecutionPolicy "Unrestricted"
-REM Set-ExecutionPolicy Unrestricted
-REM powershell.exe -ExecutionPolicy Unrestricted -File file.ps1
+REM Simple Call 1
+PowerShell.exe -ExecutionPolicy Unrestricted -File %SCRIPTPATH%
 
-PowerShell -ExecutionPolicy Bypass -File "%~dp0scriptname.ps1"
-echo Terminated.
-pause
+REM Simple Call 2
+PowerShell.exe -ExecutionPolicy Bypass -File "%SCRIPTPATH%"
+
+REM Call Script with example parameter "-ConfigFile"
+Powershell.exe -ExecutionPolicy Bypass -Command "& '%SCRIPTPATH%' -ConfigFile '%CONFIGFILE%'"
