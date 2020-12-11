b5cb1743fe8e190aad31262ba0930c1ac9a7f845
Dev <dev@wares.fr>
2020-12-11 19:35:56 +0100

    ADD DOWNLOAD/UPLOAD BLOB FUNCTIONS - added function New-BlobContent - added function Get-BlobContent - added documentation

--- Source code/Azure.ps1
+++ Source code/Azure.ps1
@@ -275,10 +275,75 @@ Function New-BlobContainer()
         
     }
     Catch{
-        Write-Error "Blob not created --> $($_.Exception.Message)" -ErrorAction:Continue
+        Write-Error "Container not created --> $($_.Exception.Message)" -ErrorAction:Continue
         return $False
     }
 
     Return $Container
+
+}
+# SUGGESTION : add parameter to allow use of -UseConnectedAccount with New-AzStorageContext for OAuth (Azure AD)
+# SUGGESTION : ValueFromPipeline = $true for $SourceFile
+Function New-BlobContent()
+{
+
+    param
+    (
+        [Parameter(Mandatory=$true)] [string] $StorageAccountName,
+        [Parameter(Mandatory=$true)] [string] $StorageAccountKey,
+        [Parameter(Mandatory=$true)] [string] $ContainerName,
+        [Parameter(Mandatory=$true)] [string] $SourceFile,
+        [Parameter(Mandatory=$true)] [string] $BlobName
+    )
+
+    Import-Module Az.Accounts
+
+    Try{
+
+        # Get Context
+        $ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+
+        # Upload
+        $Upload = Set-AzStorageBlobContent -Container $ContainerName -File $SourceFile -Blob $BlobName -Context $ctx -Force
+
+    }
+    Catch{
+        Write-Error "Blob not created --> $($_.Exception.Message)" -ErrorAction:Continue
+        return $False
+    }
+
+    Return $Upload
+
+}
+
+Function Get-BlobContent()
+{
+
+    param
+    (
+        [Parameter(Mandatory=$true)] [string] $StorageAccountName,
+        [Parameter(Mandatory=$true)] [string] $StorageAccountKey,
+        [Parameter(Mandatory=$true)] [string] $ContainerName,
+        [Parameter(Mandatory=$true)] [string] $BlobName,
+        [Parameter(Mandatory=$true)] [string] $DestinationFile
+    )
+
+    Import-Module Az.Accounts
+
+    Try{
+
+        # Get Context
+        $ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+
+        # Download
+        $Download = Get-AzStorageBlobContent -Container $ContainerName -Blob $BlobName -Destination $DestinationFile -Context $ctx -Force
+
+    }
+    Catch{
+        Write-Error "Blob not downloaded --> $($_.Exception.Message)" -ErrorAction:Continue
+        return $False
+    }
+
+    Return $Download
 
 }
