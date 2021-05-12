4f2cdd562870eb572104517c962ef2914c00f03e
Wvi @ X1 <dev@wares.fr>
2021-05-12 16:49:25 +0200

    VARIOUS TOOLS - added function IsNumeric (math.ps1) - added function Read-Module (stdio.ps1) - added function Get-EncryptedSecureString - added function Encrypt-StringWithPublicCertificate - added ps/bash oaepsha1 encryption scripts - added function Get-UnixTimeStamp - added function Get-MD5Hash - added openssl 8.66 to librariesWMI MODULE - added WMI : Watcher and CimInstanceWatcher classes + tests - reset NOTES.TXT

--- Source code/Encrypt.ps1
+++ Source code/Encrypt.ps1
@@ -65,6 +65,21 @@ function Export-EncryptedSecureString(){
 
 }
 
+function Get-EncryptedSecureString {
+
+    Param (
+
+        [Parameter(mandatory=$true)][string]$KeyFile,
+        [Parameter(mandatory=$true)][string]$StringFile
+
+    )
+
+    $Key = Get-Content $KeyFile
+    $EncryptedPassword = Get-Content $StringFile
+    Return ($EncryptedPassword | ConvertTo-SecureString -Key $Key)
+
+}
+
 function Convert-SecureStringToPlainText 
 {
 
@@ -79,3 +94,78 @@ function Convert-SecureStringToPlainText
     Return $PlainText
 
 }
+
+function Export-OaepSHA1EncryptedStringToBase64() {
+
+    param
+    (
+        [Parameter(Mandatory=$true)] [string] $PublicCertificateFilePath,
+        [Parameter(Mandatory=$true,ParameterSetName="Clear")] [string] $PlainText,
+        [Parameter(Mandatory=$true,ParameterSetName="Secure")] [securestring] $SecureString
+    )
+
+    try {
+        If ($SecureString) {$PlainText = Convert-SecureStringToPlainText $SecureString}
+        $PublicCert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($PublicCertificateFilePath)
+        $ByteArray = [System.Text.ASCIIEncoding]::ASCII.GetBytes($PlainText)
+        $EncryptedByteArray = $PublicCert.PublicKey.Key.Encrypt($ByteArray, [System.Security.Cryptography.RSAEncryptionPadding]::OaepSHA1)  
+        $EncryptedBase64String = [Convert]::ToBase64String($EncryptedByteArray)
+    }
+    catch{
+        Throw $_.exception
+    }
+
+    Return $EncryptedBase64String
+
+}
+
+Function Get-MD5Hash {
+
+    param
+    (
+        [Parameter(Mandatory=$true,ParameterSetName="File")] [string] $Filepath,
+        [Parameter(Mandatory=$true,ParameterSetName="Bytearray")] [byte] $Bytearray
+    )
+
+    try {
+        Write-Verbose "New-object MD5CryptoServiceProvider"
+        $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+        If ([string]::IsNullOrEmpty($Filepath)) {
+            Write-Verbose "Compute Hash for Bytearray"
+            return [System.BitConverter]::ToString($md5.ComputeHash($Bytearray))
+        }else {
+            If (Test-Path -path $Filepath){
+                Write-Verbose "Compute Hash for File $Filepath"
+                return [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($Filepath)))
+            }
+            else {
+                Throw [System.Management.Automation.ItemNotFoundException]
+            }
+        }
+    }
+    catch {
+        Throw $_.exception
+    }
+
+}
+
+Function Encrypt-StringWithPublicCertificate {
+
+    [CmdletBinding()]
+    [OutputType([System.String])]
+    param(
+        [Parameter(Position=0, Mandatory=$true)][ValidateNotNullOrEmpty()][System.String]
+        $ClearText,
+        [Parameter(Position=1, Mandatory=$true)][ValidateNotNullOrEmpty()][ValidateScript({Test-Path $_ -PathType Leaf})][System.String]
+        $PublicCertFilePath
+    )
+
+    # Encrypts a string with a public key
+    $PublicCert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($PublicCertFilePath)
+    $ByteArray = [System.Text.Encoding]::UTF8.GetBytes($ClearText)
+    $EncryptedByteArray = $PublicCert.PublicKey.Key.Encrypt($ByteArray,$true)
+    $EncryptedBase64String = [Convert]::ToBase64String($EncryptedByteArray)
+    
+    Return $EncryptedBase64String 
+
+} 
